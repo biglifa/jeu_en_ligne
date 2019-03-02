35ece1ce70c12d9b9f3c6a96899ede854111aae4
nina messaoudi <messaoudinina55@gmail.com>
2019-03-02 15:21:50 +0100

    insertion niveau
    a

---
+++ BD/insertion.sql
@@ -0,0 +1,9 @@
+------------------------------------------------------
+--          insertion des données de la table Niveau
+------------------------------------------------------
+
+INSERT INTO niveau values(1,'facile','00:03:00','100');
+INSERT INTO niveau values(2,'moyen','00:03:00','200');
+INSERT INTO niveau values(3,'difficile','00:03:00','300');
+
+
