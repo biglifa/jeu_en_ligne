730778005914840f018d9b6d1326e4f97a8a07a5
Fallou <bigbah007@gmail.com>
2019-03-02 16:57:01 +0100

    point
    a

--- BD/insertion.sql
+++ BD/insertion.sql
@@ -1,5 +1,5 @@
 ------------------------------------------------------
---          Insertion des données de la table Niveau
+--          Insertion des donnï¿½es de la table Niveau
 ------------------------------------------------------
 
 INSERT INTO niveau values(1,'facile','00:03:00','100');
@@ -7,13 +7,14 @@ INSERT INTO niveau values(2,'moyen','00:03:00','200');
 INSERT INTO niveau values(3,'difficile','00:03:00','300');
 
 ------------------------------------------------------
---           Insertion des données de la table
+--           Insertion des donnï¿½es de la table
 ------------------------------------------------------
 
-INSERT INTO labyrinthe VALUES(101,1,1,4,4,4,4,1)
-INSERT INTO labyrinthe VALUES(102,3,1,3,4,4,4,1)
-INSERT INTO labyrinthe VALUES(103,4,2,1,1,4,4,1)
-INSERT INTO labyrinthe VALUES(104,4,4,4,1,4,4,1)
+--
+INSERT INTO labyrinthe VALUES(101,1,1,4,4,4,4,1);
+INSERT INTO labyrinthe VALUES(102,3,1,3,4,4,4,1);
+INSERT INTO labyrinthe VALUES(103,4,2,1,1,4,4,1);
+INSERT INTO labyrinthe VALUES(104,4,4,4,1,4,4,1);
 INSERT INTO labyrinthe VALUES(105,4,1,1,4,4,4,1)
 
 
