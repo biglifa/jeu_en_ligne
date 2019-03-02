aab3504180ae728c335bc6215bacdb5fe467d770
Fallou <bigbah007@gmail.com>
2019-03-02 16:55:17 +0100

    Insertion laby niveau diff

--- BD/i.sql
+++ BD/i.sql
@@ -1,5 +1,15 @@
+--Insertion labytinthe niveau moyen
+
 INSERT INTO labyrinthe VALUES (201,5,1,4,5,5,5,2);
 INSERT INTO labyrinthe VALUES (202,1,4,3,1,5,5,2);
 INSERT INTO labyrinthe VALUES (203,5,5,2,1,5,5,2);
 INSERT INTO labyrinthe VALUES (204,1,4,1,2,5,5,2);
 INSERT INTO labyrinthe VALUES (205,1,4,2,5,5,5,2);
+
+--Insertion labytinthe niveau difficile
+INSERT INTO labyrinthe VALUES (301,6,6,4,1,6,6,3);
+INSERT INTO labyrinthe VALUES (302,2,6,5,6,6,6,3);
+INSERT INTO labyrinthe VALUES (303,1,6,6,1,6,6,3);
+INSERT INTO labyrinthe VALUES (304,3,1,3,6,6,6,3);
+INSERT INTO labyrinthe VALUES (305,1,4,1,2,6,6,3);
+
