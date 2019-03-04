690fe91447c89802af3028407322d460978eec00
Fallou <bigbah007@gmail.com>
2019-03-04 10:19:03 +0100

    ajout ; oublier

--- BD/insertion.sql
+++ BD/insertion.sql
@@ -15,7 +15,7 @@ INSERT INTO labyrinthe VALUES(101,1,1,4,4,4,4,1);
 INSERT INTO labyrinthe VALUES(102,3,1,3,4,4,4,1);
 INSERT INTO labyrinthe VALUES(103,4,2,1,1,4,4,1);
 INSERT INTO labyrinthe VALUES(104,4,4,4,1,4,4,1);
-INSERT INTO labyrinthe VALUES(105,4,1,1,4,4,4,1)
+INSERT INTO labyrinthe VALUES(105,4,1,1,4,4,4,1);
 
 
 
