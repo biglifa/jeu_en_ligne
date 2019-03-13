26e90db4782236efc7c057c3004f663e7ea5eb34
nina messaoudi <messaoudinina55@gmail.com>
2019-03-13 19:51:59 +0100

    up

---
+++ BD/proc.sql
@@ -0,0 +1,43 @@
+-------------------------------------------------------
+--procedure insertion utilisateur
+-------------------------------------------------------
+SET serveroutput ON;
+
+CREATE OR REPLACE PROCEDURE insert_util(vpseudo    utilisateur.pseudo%TYPE,
+                                        vnom       utilisateur.nom%TYPE,
+                                        vprenom    utilisateur.prenom%TYPE,
+                                        vmot_pass  utilisateur.mot_pass%TYPE)
+                                        
+IS
+BEGIN
+      INSERT INTO utilisateur VALUES(vpseudo,vnom,vprenom,vmot_pass,NULL);
+      DBMS_OUTPUT.PUT_LINE('Felicitations!,vous avez bien eté enregistré');
+      
+EXCEPTION 
+
+        WHEN DUP_VAL_ON_INDEX 
+        THEN DBMS_OUTPUT.PUT_LINE('ce pseudo existe deja ,essayez un autre');
+
+END;
+/
+
+SELECT * from niveau;
+SELECT * from cases;
+SELECT * from labyrinthe;
+SELECT * from intervenir;
+SELECT * from utilisateur;
+
+EXECUTE insert_util('nine1','messaoudi','nina','par');
+delete from utilisateur where pseudo='nine1';
+
+CREATE TRIGGER tbu_niveau_util
+after update of gagner on partie
+for each row
+begin
+if (:new.gagner=1 and :old.id_niveau<3)  then 
+update utilisateur
+set id_niveau = id_niveau++
+where pseudo
+
+
+
